00b68ca4246ec0f49834baca91bf7343748c845d
Vishnu Nanilal Panicker <vishnunlal@gmail.com>
2021-12-03 09:39:36 +0530

    Initial Unity files Commit

---
+++ Assets/Scenes/GameManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+
+    private void Awake() {
+        if(instance == null)
+            instance = this;
+    }
+}
